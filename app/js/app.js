01643c728499a3418aeae4be7d3582a1af3ff277
Dan Valinotti <dvalinotti@icloud.com>
2019-04-30 16:34:43 -0400

    update structure

--- app/js/app.js
+++ app/js/app.js
@@ -14,7 +14,10 @@ function submit() {
 
 function generate(str) {
     //console.log(firstName + " " + lastName);
+    str = str.replace(/\s/g, '');
     var fArr = str.split("");
+
+
     for (var j = 0; j < fArr.length; j += 3) {
         var colorCode = "#" + buildColor(fArr[j], fArr[j + 1], fArr[j + 2]);
         var e = document.createElement('div');
@@ -24,18 +27,26 @@ function generate(str) {
     }
 }
 
-function charToHex(ch) {
+function charToHex(ch, f) {
     if (ch != undefined) {
-        var dec = ch.charCodeAt(0);
+        var dec = Math.ceil(((ch.charCodeAt(0) - 32) / 90) * 255);
+        
     } else {
-        var dec = "00"
+        var dec = Math.ceil(((getRandomInt(32, 123) - 32) / 90) * 255);
+        console.log(dec);
     }
     return dec.toString(16);
 }
 
 function buildColor(c1, c2, c3) {
-    return "" + charToHex(c1) + charToHex(c2) + charToHex(c3);
+    return "" + charToHex(c1, 1) + charToHex(c2, 1.5) + charToHex(c3, 2);
 }
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
 
 $("#sent").keyup(function(event) {
     if (event.keyCode === 13) {
