bae32167f3bf1dae8e05fbc7f15a0bb4cd3a6252
Dan Valinotti <dvalinotti@icloud.com>
2019-04-30 17:12:47 -0400

    Document app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,22 +1,22 @@
 $(document).foundation();
 
+// Submit button function.
 function submit() {
     var oldColors = document.getElementById("colors").children;
-    if (oldColors.length > 1) {
+    if (oldColors.length > 1) {                         //Clears existing color values when resubmitting.
         for (var i = 0; i < oldColors.length; i++) {
             oldColors[i].style.display = "none";
         }
     }
-    var firstName = document.getElementById("sent").value;
-
-    generate(firstName);
+    var value = document.getElementById("sent").value;
+    document.getElementById("palette").style.display = "block";
+    generate(value);
 }
 
+// Wrapper function.
 function generate(str) {
-    //console.log(firstName + " " + lastName);
-    str = str.replace(/\s/g, '');
+    str = str.replace(/\s/g, ''); // Removes all spaces (better color generation)
     var fArr = str.split("");
-
 
     for (var j = 0; j < fArr.length; j += 3) {
         var colorCode = "#" + buildColor(fArr[j], fArr[j + 1], fArr[j + 2]);
@@ -27,25 +27,30 @@ function generate(str) {
     }
 }
 
-function charToHex(ch, f) {
+// Takes a character, transforms it to fit a 0-255 value and returns it as a Hexidecimal number.
+// Turns the character's ASCII value into a fraction of the range of available characters (32-122) and multiplies that by 255.
+function charToHex(ch) {
     if (ch != undefined) {
-        var dec = Math.ceil(((ch.charCodeAt(0) - 32) / 90) * 255);
+        var dec = Math.ceil(((ch.charCodeAt(0) - 32) / 90) * 255); 
         
     } else {
-        var dec = Math.ceil(((getRandomInt(32, 123) - 32) / 90) * 255);
+        // If the value is empty, ie there are not enough characters to build a color code, generates a random number to replace it.
+        var dec = Math.ceil(((getRandomInt(32, 123) - 32) / 90) * 255); 
         console.log(dec);
     }
     return dec.toString(16);
 }
 
+// Takes the three R G B values and builds a color code.
 function buildColor(c1, c2, c3) {
-    return "" + charToHex(c1, 1) + charToHex(c2, 1.5) + charToHex(c3, 2);
+    return "" + charToHex(c1) + charToHex(c2) + charToHex(c3);
 }
 
+// Generate a random number between min and max.
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min)) + min;
   }
 
 $("#sent").keyup(function(event) {
