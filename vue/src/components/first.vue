e26066c1a524ebe8db14814eabed72ad4a299c36
Dan Valinotti <dvalinotti@icloud.com>
2019-05-24 13:49:27 -0400

    vue update setup

---
+++ vue/src/components/first.vue
@@ -0,0 +1,20 @@
+<style scoped>
+    .demo {
+        background-color: blue;
+    }
+</style>
+
+<script>
+    export default {
+        name: 'First Component',
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+<template>
+    <div class="demo">
+        <h1>{{msg}}</h1>
+    </div>
+</template>
